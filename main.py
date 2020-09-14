fefb481b35c78ddba660e51f90e34083d631598d
atalonso <adrianotalonso@gmail.com>
2020-09-14 19:44:47 -0300

    Programa para calcular o juros composto pela formula e pelo metodo Mortgage.

---
+++ main.py
@@ -0,0 +1,18 @@
+import locale
+locale.setlocale(locale.LC_MONETARY, 'pt_br')
+from mortgage import Loan #modulo externo para o calulo do Juros
+
+from helpers import juros_compostos
+
+if __name__ == '__main__':
+    quantia_a_emprestar = float(input("Qual o valor a emprestar: "))
+    taxa_juros_anual = float(input("Qual a taxa de juros anual: "))
+    prazo = int(input("Qual o prazo: "))
+
+    valor_final_a_pagar = juros_compostos(quantia_a_emprestar, taxa_juros_anual, prazo)
+    print(f"O montante total calculado é de {locale.currency(valor_final_a_pagar, grouping=True)}.")
+
+    print()
+    print("Usando módulo Mortgage, classe Loan")
+    financiamento = Loan(quantia_a_emprestar, taxa_juros_anual/100, prazo, currency='R$')
+    print(financiamento.summarize)
